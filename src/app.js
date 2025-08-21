00b165df436c640f642bf9d341efbf05a1fe4634
DaniN-Dv <danidanovasvalor@gmail.com>
2025-08-21 01:45:25 +0000

    Excuses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = function () {
   //write your code here
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  function randWord(arr){
+    return arr[Math.floor(Math.random()*arr.length)]
+  }
+
+  let Random = `${randWord(who)} ${randWord(action)} ${randWord(what)} ${randWord(when)}`
+
   console.log("Hello Rigo from the console!");
+  
+  document.getElementById('excuse').innerHTML = Random
+  
+
 };
